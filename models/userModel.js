dd0e9b30d7645a3a2598e9b7ea0d8b47462fd17b
KMcCrear <kmccrear1@gmail.com>
2022-03-24 18:58:16 +0000

    user model, controller and routes file added

---
+++ models/userModel.js
@@ -0,0 +1,47 @@
+const nedb = require("nedb");
+
+class User {
+	constructor(dbFilePath) {
+		if (dbFilePath) {
+			//creating a concrete db to access
+			this.db = new nedb({ filename: dbFilePath, autoload: true });
+			console.log(`DB Connected to ${dbFilePath}`);
+		} else {
+			//Creating a db in memory
+			this.db = new nedb();
+		}
+	}
+	init() {
+		this.db.insert({ username: "kylemccc", password: "password" });
+
+		console.log(`db entry ${username} inserted`);
+	}
+
+	addUser(username, password) {
+		return new Promise((resolve, reject) => {
+			this.db.insert({ username: username, password: password }, (err, res) => {
+				if (err) {
+					console.log(err);
+					reject(err);
+				} else {
+					resolve(res);
+					console.log(`User ${username} successfully added to db`);
+				}
+			});
+		});
+	}
+
+	deleteUser(username) {
+		return new Promise((resolve, reject) => {
+			this.db.remove({ username: username }, (err, result) => {
+				if (err) {
+					console.log(err);
+					reject(err);
+				} else {
+					resolve(result);
+					console.log(`user ${username} successfully deleted`);
+				}
+			});
+		});
+	}
+}
